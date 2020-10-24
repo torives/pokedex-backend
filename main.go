d0a6199988a3d084e642bc486a2893cc766954a0
Victor Yves Crispim <rotcivyves@hotmail.com>
2020-10-24 16:42:01 -0300

    feat: returns first pokemon name

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	println("hello!")
+}
+
+func PokedexServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Bulbassaur")
 }
