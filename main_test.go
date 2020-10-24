c49e41cd5d6ff18921c2847d1bb7fcaf0e29c89d
Victor Yves Crispim <rotcivyves@hotmail.com>
2020-10-24 15:26:59 -0300

    test: returns first pokemon name

--- main_test.go
+++ main_test.go
@@ -1,7 +1,23 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestPokedex(t *testing.T) {
+	t.Run("returns 1st pokemon name", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/pokemon/1", nil)
+		response := httptest.NewRecorder()
 
+		PokedexServer(response, request)
+
+		got := response.Body.String()
+		want := "Bulbassaur"
+
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	})
 }
